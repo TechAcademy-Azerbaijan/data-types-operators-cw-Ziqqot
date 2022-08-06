c00d0fa51db676f3090907936b2cc2df33ab15e3
Isa <110451592+Ziqqot@users.noreply.github.com>
2022-08-06 15:05:22 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -3,9 +3,11 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-  var s=6*3**2
-  var v=3**3
-  console.log(s);
-  console.log(v);
+    var input= result.input; // "6,3"
+  var numbers=input.split(',');
+  var s=parseInt(numbers[0]);  //6
+  var v=parseInt(numbers[1]); //3
+  var sum=s*v**2
+  var sam=v**3
+  console.log(sum,sam);
 });
