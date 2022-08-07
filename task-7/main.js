e5f98762b01be29f122e5283b7e327a2036c0c42
Isa <110451592+Ziqqot@users.noreply.github.com>
2022-08-07 18:24:05 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -3,7 +3,13 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
+      let input = result.input;"3456"
+    let numbers = input.split(',');['3', '4', '5', '6']
+    let z="6"
+let x="5"
+    let c="4"
+    let v="3"
+    let zxc=z+x+c+v
+    console.log(zxc);
   
 });
