86df73657da210731c2066c0a8711a6a8390f0be
Isa <110451592+Ziqqot@users.noreply.github.com>
2022-08-07 18:47:42 +0400

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -4,13 +4,13 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-     let input=(result.input);"123,10"
+    let input=(result.input);"123,10,"
     let numbers=input.split(',');['123','10']
     let  n=parseInt([0]);"123"
     let x=parseInt([1]);"10"
-    let c=3
-    let sum=n/x;
-    let sim=parseInt(sum)+c
-    console.log(sim);
+    let c=parseInt(123/10)
+let sim="3"
+let sum=sim+c
+console.log(sum);
   
 });
