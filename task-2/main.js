0b257ce64fa93e53ae9179146ce5fde15b17f0f8
Isa <110451592+Ziqqot@users.noreply.github.com>
2022-08-08 12:32:53 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,17 +4,16 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
+
 let input= result.input;"6,3"
 let numbers=input.split(',');['6','3']
-let s=parseInt(numbers[0]);6
+let b=parseInt(numbers[0]);6
 let a=parseInt(numbers[1]);3
-let sam=s*a**2
-let sim=a**3  
+let s=b*a**2
+let v=a**3  
 
-let sum=Boolean(a*(a<=10**6));
-console.log(sam);
-console.log(sim);
-console.log(sum);
+console.log(s);
+console.log(v);
 
 
   
