2d6bb6480c522e1e20699db922024b8ec1dc0c5b
Isa <110451592+Ziqqot@users.noreply.github.com>
2022-08-09 14:04:06 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,14 +4,7 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-let input=(result.input);"2,10"
-let numbers=input.split(',');['2','10']
-
-let a=parseInt(numbers[0]);"2"
-let n=parseInt(numbers[1]);"10"
-
-let sum=a**n;
-
-console.log(sum);
-  
+let i=(result.input);
+let a=(i**10);
+console.log(a);
 });
