7c95bf5b2cc92ac30368da8edb88e8c46b67cce9
Isa <110451592+Ziqqot@users.noreply.github.com>
2022-08-09 14:46:19 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -4,12 +4,9 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    let input=(result.input);"27,10"
-let numbers=input.split(',');['27','10']
-let m=parseInt(numbers[0]);"27"
-let n=parseInt(numbers[1]);"10"
-
-let sum=m/n;
+ let i=parseInt(result.input)
+let a=parseInt(i/10);
+console.log("Cavab",a);
 
 console.log(sum);
   
