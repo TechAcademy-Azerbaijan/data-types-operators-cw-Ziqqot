8682bb87990b2f0c73a512f2f1eaeb51b62b28ca
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-13 15:23:16 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -3,12 +3,13 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-let input=result.input;
-let number=input.split(" ");
+
+  let input=result.input;
+let number=input.split(",");
 let x=parseInt(number[0]);
 let n=parseInt(number[1]);
 let m=parseInt(number[2]); 
-let cvb=(x**n)%m;
+let cvb=(x**n)%m
 console.log(cvb);
 });
 
