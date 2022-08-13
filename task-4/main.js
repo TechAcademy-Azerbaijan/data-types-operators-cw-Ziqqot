3314be67dd1e36d49b823c62ea9f301ce124461e
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-13 15:38:25 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -4,9 +4,12 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
- let i=parseInt(result.input)
-let m=parseInt(i/10);
-console.log(m);
+ let input=result.input;
+    let number=input.split(',');
+    let m=parseInt(number[0]);
+    let n=parseInt(number[1]);
+    let res=parseInt(m/n);
+    console.log(res);
 
 
   
